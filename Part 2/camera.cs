28308a4f3d0bc14adcf568337b27dcef2298c65b
Mika Peltomaa <mika@Mika-MacBook-Pro.local>
2016-09-22 09:16:23 +0300

    Part 2

---
+++ Part 2/camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	public GameObject ship;
+	private Vector3 cameraOffset;
+
+	void Start () {
+		cameraOffset = transform.position - ship.transform.position;
+	}
+	
+	void Update () {
+		transform.position = ship.transform.position + cameraOffset;
+	}
+}
---
+++ Part 2/diamond.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diamond : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Destroy (gameObject);
+	}
+}
