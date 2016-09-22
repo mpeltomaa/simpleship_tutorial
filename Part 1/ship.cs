2653a3b729034622996a4dfa8c3e765c3ae66c8b
Mika Peltomaa <mika@Mika-MacBook-Pro.local>
2016-09-22 09:14:27 +0300

    Part 1

---
+++ Part 1/ship.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ship : MonoBehaviour {
+
+	void Update () {
+
+		// Fire thrusters with key W
+		if (Input.GetKey (KeyCode.W)) {
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * 10);
+		}
+
+		// Turn left with key A
+		if (Input.GetKey(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().AddTorque(1f);
+		}
+
+		// Turn right with key D
+		if (Input.GetKey(KeyCode.D))
+		{
+			GetComponent<Rigidbody2D>().AddTorque(-1f);
+		}
+
+	}
+
+}
